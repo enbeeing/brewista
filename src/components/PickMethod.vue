6aa91a00ee0c89cc980a8b7943cec10074919deb
enbeeing <eddykjllr@hotmail.com>
2022-06-21 11:41:02 +0200

    singleMethod (first draft)

---
+++ src/components/PickMethod.vue
@@ -0,0 +1,59 @@
+<template>
+  <PickHeader :title="'Combine with method'" />
+  <section>
+    <div v-for="method in methods" :key="method.title">
+      <SingleMethod :method="method" />
+    </div>
+  </section>
+</template>
+
+<script setup>
+import PickHeader from "./PickHeader.vue";
+import SingleMethod from "./SingleMethod.vue";
+
+const methods = [
+  {
+    title: "French Press",
+    svg: "#french",
+    method: {
+      gram: "33g",
+      water: "500ml",
+      grind: "Coarse",
+      time: "4min",
+    },
+  },
+  {
+    title: "Cold Infusion",
+    svg: "#infusion",
+    method: {
+      gram: "40g",
+      water: "200ml",
+      grind: "Coarse",
+      time: "2 timer",
+    },
+  },
+  {
+    title: "Chemex",
+    svg: "#chemex",
+    method: {
+      gram: "16g",
+      water: "230ml",
+      grind: "Fine",
+      time: "3min",
+    },
+  },
+  {
+    title: "Kalita Wave",
+    svg: "#kalita",
+    method: {
+      gram: "16g",
+      water: "230ml",
+      grind: "Fine",
+      time: "3min",
+    },
+  },
+];
+</script>
+
+<style>
+</style>
---
+++ src/components/SingleMethod.vue
@@ -0,0 +1,68 @@
+<template>
+  <article class="method content-wrapper">
+    <div>
+      <svg class="method-svg">
+        <use :xlink:href="method.svg" />
+      </svg>
+    </div>
+    <div class="method-content">
+      <h3>
+        {{ method.title }}
+      </h3>
+      <div class="brew-method">
+        <p>
+          <svg class="method-icon">
+            <use xlink:href="#bean" />
+          </svg>
+          {{ method.method.gram }}
+        </p>
+        <p>
+          <svg class="method-icon">
+            <use xlink:href="#water" />
+          </svg>
+          {{ method.method.water }}
+        </p>
+        <p>
+          <svg class="method-icon">
+            <use xlink:href="#grind" />
+          </svg>
+          {{ method.method.grind }}
+        </p>
+        <p>
+          <svg class="method-icon">
+            <use xlink:href="#time" />
+          </svg>
+          {{ method.method.time }}
+        </p>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script setup>
+const props = defineProps({
+  method: Object,
+});
+</script>
+
+<style>
+.method {
+  background: var(--brew-bg-color);
+  margin: 2rem 0;
+  border-radius: 25px;
+}
+
+.method .method-svg {
+  margin-right: 1rem;
+}
+
+.method-content {
+  width: 100%;
+}
+
+.method .brew-method > p,
+.method .brew-method > p:first-of-type {
+  margin-top: 0.5rem;
+  margin-left: 0;
+}
+</style>
