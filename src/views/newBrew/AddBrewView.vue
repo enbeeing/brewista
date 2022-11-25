121b0154f2b963c87b0f19673c2570f02ddb7efa
enbeeing <eddykjllr@hotmail.com>
2022-11-25 11:59:43 +0100

    saving before the weekend

--- src/views/newBrew/AddBrewView.vue
+++ src/views/newBrew/AddBrewView.vue
@@ -1,23 +1,41 @@
 <template>
-  <div class="new-brew">
-    <span class="error" v-if="error">{{ error }}</span>
-    <h3>Chosen Coffee:</h3>
-    <p>{{ coffee.title }}</p>
-    <h3>Chosen Method:</h3>
-    <p>{{ method.title }}</p>
-    <h4>Upload image:</h4>
-    <form>
+  <div class="new-brew add-brew">
+    <div class="error" v-if="error">
+      <p>{{ error }}</p>
+    </div>
+
+    <Header title="Post Brew" />
+    <!-- send home true to the header, 
+      show home btn instead of back arrow -->
+    <!-- add leave/home button, 
+      ask if they are sure and
+      inform that they'll loose all the brew data -->
+
+    <form class="add-brew-form content-wrapper">
+      <h4>Upload image</h4>
       <label class="sr-only" for="img">Image upload</label>
-      <input type="file" @change="onFile" id="img" />
+      <input type="file" @change="onFile" id="img" required />
       <label for="alt">Describe image: </label>
       <input type="text" id="alt" v-model="imgAlt" />
+      <button class="add-brew" @submit="addNewBrew">Add</button>
     </form>
 
-    <button class="add-brew" @click="addNewBrew">Add</button>
+    <div>
+      <h4>Chosen Method</h4>
+      <SingleMethod :method="method" />
+    </div>
+
+    <div>
+      <h4>Chosen Coffee</h4>
+      <SingleCoffee :coffee="coffee" />
+    </div>
   </div>
 </template>
 
 <script setup>
+import Header from "../../components/newBrew/PickHeader.vue";
+import SingleCoffee from "../../components/newBrew/SingleCoffee.vue";
+import SingleMethod from "../../components/newBrew/SingleMethod.vue";
 import addBrew from "../../composables/addBrew";
 import uploadImg from "../../composables/fileUpload/uploadImg";
 import { ref } from "vue";
@@ -49,7 +67,6 @@ const onFile = (e) => {
 
   imgSrc.value = files[0].name;
 
-  // how to save into folder?
   imgFile.value = files[0];
 };
 
@@ -80,4 +97,47 @@ const addNewBrew = () => {
 </script>
 
 <style>
+.add-brew .error {
+  position: absolute;
+  top: 2.5rem;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 200px;
+  background: var(--brew-bg-color);
+  padding: 1rem;
+  border-radius: 10px;
+  color: var(--error-color);
+  font-weight: bold;
+  text-align: center;
+}
+
+.add-brew > div > h4 {
+  text-align: center;
+  margin-bottom: -1rem;
+}
+
+.add-brew-form {
+  background: var(--brew-bg-color);
+  border-radius: 25px;
+  margin: 2rem 0;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(5, auto);
+  gap: 10px;
+}
+
+.add-brew-form h4 {
+  text-align: center;
+  margin-bottom: 0.5rem;
+}
+
+.add-brew-form label {
+  font-size: 0.9rem;
+}
+
+.add-brew .coffee,
+.add-brew .method {
+  cursor: default;
+}
 </style>
